8d07d38219dcfecf20799f3001bfb4493ea23e4f
liyustar <liyustar@gmail.com>
2016-03-11 19:29:53 -0800

    update install_aptget

--- linuxconf/install_aptget.sh
+++ linuxconf/install_aptget.sh
@@ -3,7 +3,9 @@
 
 set -e
 
-INSTALL_CMD="apt-get install"
+INSTALL_CMD="apt-get install -y"
+
+$INSTALL_CMD zsh
 
 $INSTALL_CMD git
 $INSTALL_CMD vim
@@ -11,3 +13,8 @@ $INSTALL_CMD vim
 $INSTALL_CMD tmux
 
 $INSTALL_CMD htop
+$INSTALL_CMD iotop
+$INSTALL_CMD iftop
+$INSTALL_CMD iptraf
+$INSTALL_CMD slurm
+$INSTALL_CMD dstat
